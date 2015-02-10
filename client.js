b6dbc598d2c18795f3959b1d309531fc17278050
Alper Tayfun <alper.tayfun@hotmail.com>
2015-02-10 20:08:37 +0200

    delete handshake query information
    I'll adding some new features follow days.

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var socket = io.connect('https://www.domain.com:1313/', { query: "user="+usr },{secure: true}); //set our domain or server ip and port
+    var socket = io.connect('https://www.domain.com:1313/',{secure: true}); //set our domain or server ip and port
 
     socket.on('notification', function (data) {
     	console.log(data); //lets get the data.
